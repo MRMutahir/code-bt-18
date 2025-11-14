04e5870addac1a3642660032efb290b3d28d9a6b
MRMutahir <mutahirkareem820@gmail.com>
2025-11-14 12:36:35 +0500

    array complete

---
+++ javascript/class5 array/array.md
@@ -0,0 +1,97 @@
+
+
+🔥 Slice Practice Questions (non-destructive)
+
+Q1:
+
+Ek array hai:
+[5, 10, 15, 20, 25, 30]
+Isme se sirf 3rd se 5th element tak ka new array banao.
+
+
+---
+
+Q2:
+
+Ek array:
+["a", "b", "c", "d", "e"]
+Isme se "b", "c", "d" ko slice karke naya array banao.
+
+
+---
+
+Q3:
+
+Array:
+[1, 2, 3, 4, 5]
+Isme se last 2 elements ko slice karke new array banao.
+
+
+---
+
+Q4:
+
+Array:
+["red", "blue", "green", "yellow"]
+Isme se index 1 se end tak ka slice banao.
+
+
+---
+
+Q5:
+
+Array:
+[100, 200, 300, 400, 500]
+Is array ko copy karo using slice().
+
+
+---
+
+
+---
+
+🔥 Splice Practice Questions (destructive)
+
+Q6:
+
+Array:
+[10, 20, 30, 40, 50]
+Isme se 20 aur 30 ko splice karke remove karo.
+
+
+---
+
+Q7:
+
+Array:
+["Ali", "Bilal", "Hassan"]
+"Bilal" ko hatake uski jagah
+"Umer", "Usman" insert karo.
+
+
+---
+
+Q8:
+
+Array:
+[1, 2, 3, 4, 5]
+Isme se 3rd element delete karo using splice.
+
+
+---
+
+Q9:
+
+Array:
+["car", "bike", "bus"]
+Index 1 per "train" add karo using splice.
+
+
+---
+
+Q10:
+
+Array:
+[2, 4, 6, 8, 10]
+Isme se middle ke 6, 8 remove kar ke ek hi value
+99 insert karo.
---
+++ javascript/class5 array/index.js
@@ -0,0 +1,63 @@
+// // var arr = [];
+// // // var getName = prompt("enter user name?");
+// // console.log("outsider", arr.length);
+// // if (arr.length <= 10) {
+
+// //   arr[arr.length] = prompt("enter user name?");
+// //   console.log(arr.length);
+
+// //   arr[arr.length + 1] = prompt("enter user name?");
+// //   console.log(arr.length);
+
+// //   console.log(arr);
+
+// //   //   if (arr.length == 4) {
+// //   //     alert("admision close");
+// //   //     console.log(arr);
+// //   //   }
+// // }
+
+// var cars = ["Saab", "Volvo", "BMW"];
+
+// // // cars.push("Toyota");
+// // cars.pop()
+
+// cars.unshift("Civic");
+
+// console.log(cars);
+
+// splice  and slice
+
+// var cars = ["Saab", "Volvo", "BMW", "Toyota", "Tesla", "GWM"];
+// // var user =  2
+// // const removeEle =  cars.splice(2, 1, user);
+
+// console.log(cars);
+// console.log(typeof cars)
+
+// // console.log(removeEle);
+
+// // var a =  2
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// // const newValue = numbers.splice(3,1)
+
+// // console.log(numbers)
+
+// var userdata = {
+//   user: "Muhammad",
+//   email: "mutahir@gmail.com",
+// };
+
+// console.log(userdata);
+
+// var db = ["Saab", "Volvo", "BMW", "Toyota", "Tesla", "GWM"];
+
+
+// var newEle = cars.slice(2, 4);
+
+// // var newEle = cars.splice(2,2);
+
+// console.log("cars >>>>>>>>>>", cars);
+// console.log("newEle >>>>>>>>>>>> ", newEle);
