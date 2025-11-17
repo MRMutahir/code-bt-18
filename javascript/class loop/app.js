70d4c2c848ead244a968eb3190070f9ee0b7c5a1
MRMutahir <mutahirkareem820@gmail.com>
2025-11-17 13:02:46 +0500

    add loop

---
+++ javascript/class loop/app.js
@@ -0,0 +1,13 @@
+// var cleanestCities = ["Karachi", "Lahore", "Multan", "Kashmir", "Peshawar"];
+
+var tableNumber = Number(prompt("enter your city name?"));
+
+for (var i = 1; i <= 10; i++) {
+  document.writeln(`<h1>${tableNumber} * ${i} = ${tableNumber * i}</h1><br>`);s
+}
+
+// console.log(2 + "*" + 1 + "=" + 1 * 2);
+// console.log(2 + "*" + 2 + "=" + 2 * 2);
+// console.log(2 + "*" + 3 + "=" + 3 * 2);
+// console.log(2 * 2);
+// console.log(3 * 2);
