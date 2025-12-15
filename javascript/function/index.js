aa8e6cb724a96b30de1659e5035ea61e68612616
MRMutahir <mutahirkareem820@gmail.com>
2025-12-15 13:02:54 +0500

    function start

---
+++ javascript/function/index.js
@@ -0,0 +1,17 @@
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// console.log("time: " + theHr + ":" + theMin);
+
+// function myDate(userName, age, email) {
+//   console.log(userName);
+//   console.log(age);
+//   console.log(email);
+// }
+
+// myDate("sarim 30 sarim@gmail.com");
+// myDate("saboor", 20, "sabor@gmail.com");
+// myDate("hamdan", 50, "ahamdan@gmail.com");
+   
+
+
