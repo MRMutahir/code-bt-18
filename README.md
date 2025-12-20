ef58d58992167a850bca8818a3a26e43748139af
MRMutahir <mutahirkareem820@gmail.com>
2025-12-20 17:54:49 +0500

    javascript start

--- README.md
+++ README.md
@@ -30,3 +30,70 @@ node_modules/
 .env
 dist/
 .DS_Store
+
+
+
+
+
+Abdul muhid 14
+
+Abdul rafay 8
+Abdul qadir 6
+Abrar ahmed 3
+
+Ahmed 5
+Ahmed Anis 4
+
+Ahmed raza 26
+
+Ali 8
+
+Ali akber 8
+
+Aqib waheed 7
+Areeb Ahmed 10
+Ayaan Imran 16
+
+Hamza 10
+
+M MunibQadri 13
+
+Momin Khan 27
+
+Muhammad Arsalan 3
+
+Muhammad Faizan Hussain 17
+
+Muhammad Fuzail Raza 10
+Muhammad Hassan  3
+Muhammad Ikrash 4
+Muhammad Imtiaz Ahmed 12
+
+Muhammad Kashif 1
+
+Muhammad Owais 1
+
+Muhammad Sami 1
+
+Muhammad Taha Rauf 5
+
+Muhammad Talha Arif 12
+
+Muhammad saad 2
+
+Muzammil 10
+
+Shahzain 8
+
+Syed Huzaifa Shah 33
+
+Talha 13
+
+Umer  2
+
+Umer Karwaan 17
+
+shahmeer 8
+
+ali raza 12
+```
--- javascript/class1/index.js
+++ javascript/class1/index.js
@@ -1,1 +1,50 @@
-var resultOfComputation =( 2 * 4) * (4 + 2);
+// var mutahir = ""; /* string*/
+// ""``;
+// ("2");
+
+// 255; /*Number */
+
+// true;
+// false; /*Boolean */
+
+// undefined; /*Undefined */
+
+// { name: "meraName", age: 21, number: "2910" }; // returns "object"
+
+//  ['Mkahdo',5455,true,undefined,{},[]]; // returns "Array"
+
+// var meranumber = 55;
+// var meraname = "Muhammad Mutahir";
+
+// console.log(meraname);
+// console.log(meranumber);
+
+// alert(meraname);
+// alert(meranumber);
+
+// document.writeln(meraname);
+// document.writeln(meranumber);
+
+// console.log(typeof meranumber);
+
+// console.log(typeof meraname);
+// console.log(meraname);var age
+
+// Legal Variable Names
+// Start with Letter/Underscore: age, _count, myVariable.
+// Contain Letters, Numbers, Underscores: count1, user_id, totalAmount.
+// Case-Sensitive: age, Age, and AGE are different variables.
+// Can Use Keywords (but not be them): myVar is fine, but var is a keyword.
+
+// var $name;
+
+// Naming Conventions (for readability)
+// Camel Case: myVariableName (common in JavaScript/Java).
+// Pascal Case: MyVariableName (common in C#).
+// Snake Case: my_variable_name (common in Python/C).
+
+var myname = "Muhammad";
+console.log(typeof "Muhammad");
+// var nAme = 'h',0
+
+// console.log("o jani kiya hal hen", 45, true, false, {}, []);
