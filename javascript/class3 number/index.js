caaa0a3ec8489ea631a3f253363c05eef064f155
MRMutahir <mutahirkarem820@gmail.com>
2025-12-30 17:55:57 +0500

    Math expressions:

--- javascript/class3 number/index.js
+++ javascript/class3 number/index.js
@@ -1,103 +1,168 @@
-// // // // // var user_num_1 = "20"
-// // // // // var user_num_2 = 2
-// // // // // var all_sum = user_num_1   +  user_num_2
+// // // // // // var user_num_1 = "20"
+// // // // // // var user_num_2 = 2
+// // // // // // var all_sum = user_num_1   +  user_num_2
 
-// // // // // console.log("all_sum : >>>>>> ", all_sum)
+// // // // // // console.log("all_sum : >>>>>> ", all_sum)
 
-// // // // // Legal variable names
-// // // // var __username__ = ""
-// // // // var USERNAME = ""
-// // // // var username = ""
+// // // // // // Legal variable names
+// // // // // var __username__ = ""
+// // // // // var USERNAME = ""
+// // // // // var username = ""
 
-// // // // var username1
-// // // // var username_1
-// // // // var __user__name_1
+// // // // // var username1
+// // // // // var username_1
+// // // // // var __user__name_1
 
-// // // // var $user_name = ""
+// // // // // var $user_name = ""
 
-// // // var name1 = "abc"
-// // // var name1 = "efg"
-// // // var name1 = "ijk"
+// // // // var name1 = "abc"
+// // // // var name1 = "efg"
+// // // // var name1 = "ijk"
 
-// // // console.log(name1)
-// // // console.log(name1)
+// // // // console.log(name1)
+// // // // console.log(name1)
 
-// // // var user_name = "Muhammad"
+// // // // var user_name = "Muhammad"
 
-// // // user_name = "Mutahir"
+// // // // user_name = "Mutahir"
 
-// // // console.log(user_name)
+// // // // console.log(user_name)
 
-// // var spec = prompt("Your species?", "human");
-// // console.log(spec)
+// // // var spec = prompt("Your species?", "human");
+// // // console.log(spec)
 
-// // var number = 8
-// // var new_num = ++number
-// // console.log("1",number)
-// // console.log("2",new_num)
-// // console.log("3",number)
+// // // var number = 8
+// // // var new_num = ++number
+// // // console.log("1",number)
+// // // console.log("2",new_num)
+// // // console.log("3",number)
 
-// // ++name
-// // name++
+// // // ++name
+// // // name++
 
-// // --name
-// // name--
+// // // --name
+// // // name--
 
-// // var b = 4; // 4
+// // // var b = 4; // 4
 
-// // var c  = b--;
+// // // var c  = b--;
 
-// // console.log("c",c)
+// // // console.log("c",c)
 
-// // var d = c--;
+// // // var d = c--;
 
-// // console.log("d", d);
+// // // console.log("d", d);
 
-// // var e = d--;
+// // // var e = d--;
 
-// // console.log("e", e); // 4
+// // // console.log("e", e); // 4
 
-// // console.log("d", d); // 3
+// // // console.log("d", d); // 3
 
-// // var a = 2;
-// // // a  > 2   ++a  3
-// // var b = ++a; // 3
-// // console.log(b)
+// // // var a = 2;
+// // // // a  > 2   ++a  3
+// // // var b = ++a; // 3
+// // // console.log(b)
 
-// // var c = ++b // 4
-// // console.log(b)
+// // // var c = ++b // 4
+// // // console.log(b)
 
-// // //c > 4
+// // // //c > 4
 
-// // console.log(c)
+// // // console.log(c)
 
-// // var a = 2
-// // console.log(a++)
-// // console.log(a)
+// // // var a = 2
+// // // console.log(a++)
+// // // console.log(a)
 
-// // var x = 7;
-// // x++;  // 7
-// // ++x;  // 8
-// // // console.log(x);
+// // // var x = 7;
+// // // x++;  // 7
+// // // ++x;  // 8
+// // // // console.log(x);
 
-// // var x = 3;
-// // console.log(x++ + ++x);  /// Abdullah
+// // // var x = 3;
+// // // console.log(x++ + ++x);  /// Abdullah
 
-// // var x = 5;
-// // // var y = x++ + ++x + x;
-// // // console.log(y); // zohaib
+// // // var x = 5;
+// // // // var y = x++ + ++x + x;
+// // // // console.log(y); // zohaib
 
-// // resultOfComputation = (2 * 4) * (4 + 2);
+// // // resultOfComputation = (2 * 4) * (4 + 2);
 
-// // console.log("resultOfComputation >>>>>>>>>>>>>>>>",resultOfComputation)
+// // // console.log("resultOfComputation >>>>>>>>>>>>>>>>",resultOfComputation)
 
-// // var user_value_1 = Number(prompt("enter your first value?"));
-// // var convert_into_str = toString(user_value_1)
-// // console.log(typeof user_value_1)
-// // console.log(typeof convert_into_str)
+// // // var user_value_1 = Number(prompt("enter your first value?"));
+// // // var convert_into_str = toString(user_value_1)
+// // // console.log(typeof user_value_1)
+// // // console.log(typeof convert_into_str)
 
+// // value1 + value2
+// // value1 - value2
+// // value1 * value2
+// // value1 / value2
 
-// value1 + value2
-// value1 - value2
-// value1 * value2
-// value1 / value2
+// // var meraNumber = 10;
+// // var meraNumber2 = 10;
+// // var newSum = meraNumber + meraNumber2;
+// // var newSum1 = meraNumber - meraNumber2;
+// // var newSum2 = meraNumber * meraNumber2;
+// // var newSum3 = meraNumber / meraNumber2;
+
+// // console.log(newSum);
+// // console.log(newSum1);
+// // console.log(newSum3);
+
+// // var num1 = Number(prompt("enter your number"));
+// // var num2 = Number(prompt("enter your number"));
+// // console.log(typeof num1);
+// // console.log(typeof num2);
+// // var sumValue = num1 + num2;
+
+// // console.log(sumValue);
+
+// // var userFName = "Muhammad";
+// // var userLName = "Mutahir";
+// // console.log(userFName + userLName);
+
+// // var myNumber = "hi";
+// // var newNumber = Number(myNumber);
+// // console.log(newNumber);
+// // console.log(typeof newNumber);
+
+// prompt("enter your f name");
+// prompt("enter your l name");
+
+// +
+
+// prompt("enter your f name");
+// prompt("enter your l name");
+
+// -
+
+// prompt("enter your f name");
+// prompt("enter your l name");
+
+// *
+
+// prompt("enter your f name");
+// prompt("enter your l name");
+
+// /
+// var user_f_name = prompt("enter your f name");
+// var user_l_name = prompt("enter your l name");
+// var mySpace = " ";
+// console.log(user_f_name);
+// console.log(user_l_name);
+
+// console.log("Wellcome" + mySpace + user_f_name + mySpace + user_l_name);
+
+// var num1 = Number(prompt("enter first number"));
+// var num2 = Number(prompt("enter second number"));
+// console.log(num1);
+// console.log(num2);
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// var mySum = num1 + num2
+// console.log("Sum is : >>>> ", mySum);
