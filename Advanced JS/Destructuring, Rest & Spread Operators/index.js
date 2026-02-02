d9a12c65635160f817c104373341777ac3b917d0
Javeria-Hassan-SE <javeria.hassan2601@gmail.com>
2026-02-02 11:17:18 +0500

    add async

--- Advanced JS/Destructuring, Rest & Spread Operators/index.js
+++ Advanced JS/Destructuring, Rest & Spread Operators/index.js
@@ -1,46 +1,46 @@
-// let user = {
-//   name: "Muhammad",
-//   age: 21,
-//   school: "smit",
-// };
+// // let user = {
+// //   name: "Muhammad",
+// //   age: 21,
+// //   school: "smit",
+// // };
 
-// let arr = [20, 28];
+// // let arr = [20, 28];
 
-// const { name, age, school } = user;
-// const [num1, num2] = arr;
+// // const { name, age, school } = user;
+// // const [num1, num2] = arr;
 
-// // console.log(name);
-// // console.log(age);
-// // console.log(school);
-// console.log(num1);
-// console.log(num2);
+// // // console.log(name);
+// // // console.log(age);
+// // // console.log(school);
+// // console.log(num1);
+// // console.log(num2);
 
-// // const name = user.name
-// // const age = user.age
-// // const school = user.school
+// // // const name = user.name
+// // // const age = user.age
+// // // const school = user.school
 
-let arr = [10, 20, 30, 40, 50, 60, 70];
+// let arr = [10, 20, 30, 40, 50, 60, 70];
 
-// let [...a , ...b] = arr;
-console.log(a);
-// console.log(arr1);
-// console.log(arr2);
-// console.log(arr3);
-// console.log(restMe);
+// // let [...a , ...b] = arr;
+// console.log(a);
+// // console.log(arr1);
+// // console.log(arr2);
+// // console.log(arr3);
+// // console.log(restMe);
 
-// let userProfile = {
-//   name: "Muhammad",
-//   age: 21,
-// };
+// // let userProfile = {
+// //   name: "Muhammad",
+// //   age: 21,
+// // };
 
-// let userSchooling = {
-//   subject: ["DevOps", "Backend", "AWS"],
-//   garde: "A",
-// };
+// // let userSchooling = {
+// //   subject: ["DevOps", "Backend", "AWS"],
+// //   garde: "A",
+// // };
 
-// let compleetProfile = {
-//   ...userProfile,
-//   ...userSchooling,
-// };
+// // let compleetProfile = {
+// //   ...userProfile,
+// //   ...userSchooling,
+// // };
 
-// console.log(compleetProfile)
+// // console.log(compleetProfile)
